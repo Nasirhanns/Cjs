af6716dccb1911ae2acb6d0286c8466b017122af
Medusaa <am1tahunprem@gmail.com>
2024-12-11 05:22:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-global.owner = ['62895331520602']  
-global.mods = ['62895331520602'] 
-global.prems = ['62895331520602']
-global.nameowner = 'Tio'
-global.numberowner = '62895331520602' 
-global.mail = 'support@tioprm.eu.org' 
-global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
-global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
+global.owner = ['6282353035070']  
+global.mods = ['6282353035070'] 
+global.prems = ['6282353035070']
+global.nameowner = 'Nasirxml'
+global.numberowner = '6282353035070' 
+global.mail = 'amprem1tahun@gmail.com' 
+global.gc = ''
+global.instagram = 'https://instagram.com/_sirrrr'
+global.wm = '© Nasirxml'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made With'
 global.author = 'Bot WhatsApp'
 global.autobio = false // Set true untuk mengaktifkan autobio
-global.maxwarn = '3' // Peringatan maksimum
+global.maxwarn = '5' // Peringatan maksimum
 global.antiporn = true // Auto delete pesan porno (bot harus admin)
 
 //INI WAJIB DI ISI!//
